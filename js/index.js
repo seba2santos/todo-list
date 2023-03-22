70a46826f28b7c166984d4a7d48a3d6b6abfa0bb
Sebastian dos Santos <seba2santos@live.com>
2023-03-22 09:10:29 -0300

    error-message if is empty the input & eliminate task

--- js/index.js
+++ js/index.js
@@ -4,17 +4,31 @@ const $task = d.querySelector("#task");
 
 d.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (e.target.matches("#contact-form")) {
-    const $ul = d.querySelector("#tasks-todo");
-    const $li = d.createElement("li");
-    $li.innerHTML = $task.value;
-    $li.setAttribute("id", $task.value);
-    const $btn = d.createElement("span");
-    $btn.dataset.id = $li.id;
-    $btn.innerHTML = `<i class="fa-regular fa-circle-check"></i>`;
-    $li.appendChild($btn);
-    $ul.appendChild($li);
+  const $ul = d.querySelector("#tasks-todo");
+  const $li = d.createElement("li");
+  const $errorMsg = d.querySelector(".none");
+  let id = new Date().getTime().toString();
 
-    $task.value = "";
+  if (e.target.matches("#contact-form")) {
+    if ($task.value !== "") {
+      $errorMsg.classList.remove("is-active");
+      $li.textContent = $task.value;
+      $li.setAttribute("id", id);
+      const $btn = d.createElement("span");
+      $btn.dataset.id = $li.id;
+      $btn.innerHTML = `<i class="fa-regular fa-circle-check .down"></i>`;
+      $li.appendChild($btn);
+      $ul.appendChild($li);
+      $task.value = "";
+    } else {
+      $errorMsg.classList.add("is-active");
+    }
   }
+  d.addEventListener("click", (e) => {
+    if (e.target.matches("#tasks-todo span *")) {
+      let span = e.target.parentNode;
+      let li = d.getElementById(span.getAttribute("data-id"));
+      $ul.removeChild(li);
+    }
+  });
 });
